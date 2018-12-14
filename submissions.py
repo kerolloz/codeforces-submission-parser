f5d7027c3a0075db16c6cc8c12dc410e88739375
Kerollos Magdy <36763164+kerolloz@users.noreply.github.com>
2018-12-14 21:56:44 +0200

    erros fixed :100:

--- submissions.py
+++ submissions.py
@@ -3,12 +3,13 @@ import urllib.request
 import os
 import re
 
+
 try:
     from bs4 import BeautifulSoup
 except Exception as e:
     print(e, "\n------------------------------------------------"
              "\nYout should have BeautifulSoup installed\n"
-             "You can use the followng command to install it\n",
+             "You can use the following command to install it\n",
           "sudo apt-get install python3-bs4\n"
           "------------------------------------------------"
           )
@@ -19,21 +20,30 @@ soup = None
 
 def parse_it(div_class, file_name):
     global soup
+    element = "input" if file_name == "in" else "output"
+    file_name = "." + file_name
     test_number = 0
     for div in soup.find_all("div", div_class):
         test_number += 1
+        print(">> Parsing", element, "#", test_number)
         with open(str(test_number) + file_name, 'w') as out_file:
-        	out_file.writelines(div.contents[len(div.contents) - 2].pre.contents)
-           
-    os.chdir("..")
+            out_file.writelines(div.contents[len(div.contents) - 2].pre.contents)
+
     return test_number
 
 
 def main():
     problem_link = input(">> Enter Submission Link: ")
+    print(">> Please wait while downloading the submission content")
+    try:
+        my_request = urllib.request.urlopen(problem_link)
+        my_html = my_request.read()
+    except Exception as __connection_error__:
+        print(">> Something went wrong while reading the submission link!", __connection_error__)
+        return -1
 
-    my_request = urllib.request.urlopen(problem_link)
-    my_html = my_request.read()
+    print(">> Submission HTML Downloaded successfully")
+    print(">> Extraction input and output from submission HTML")
 
     numbers_in_link = re.findall("([0-9]+)", problem_link)  # numbers
 
@@ -45,7 +55,8 @@ def main():
     # create a directory named after the submission number and contest number
     os.system("mkdir " + dir_name)
     os.chdir("./" + str(dir_name))
-    os.system("mkdir test_cases && cd '$_'")
+    os.system('mkdir test_cases')
+    os.chdir("./test_cases")
 
     global soup
     soup = BeautifulSoup(my_html, "html.parser")
@@ -56,9 +67,9 @@ def main():
         print(">> DONE!",
               "\n>> Submission has been parsed Successfully to", dir_name, "!"
               )
-        os.system("cd ..")
+        os.chdir("..")
         with open("number_of_test_cases.txt", "w") as n_ts:
-            n_ts.writeline(number_of_testcases)
+            n_ts.write(str(number_of_testcases))
     else:
         print(">> Something went WRONG!")
 
